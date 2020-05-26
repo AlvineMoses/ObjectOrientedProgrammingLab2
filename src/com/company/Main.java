02a614fd091de78fa10530e55cbc1f40520ad97c
AlvineMoses <alvine.mburu@strathmore.edu>
2020-05-26 14:48:27 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,28 @@
+package com.company;
+
+/*
+    Group Members
+    100994 Kimani Martin Gitau
+    088462 Alvine Moses
+ */
+public class Main{
+    public static void main(String[] args) {
+        Sedan mySedan = new Sedan();
+        mySedan.licencePlate = "8904 UIRJ";
+        mySedan.maxSpeed = 150;
+        mySedan.gasTank = 78;
+        mySedan.capacity = 4;
+        mySedan.speed = 0;
+        mySedan.myCar();
+
+        System.out.println(" ");
+
+        SportsCar mySportsCar = new SportsCar();
+        mySportsCar.licencePlate = "8632 IHND";
+        mySportsCar.maxSpeed = 300;
+        mySportsCar.gasTank = 0;
+        mySportsCar.capacity = 2;
+        mySportsCar.speed = 0;
+        mySportsCar.myCar();
+    }
+}
